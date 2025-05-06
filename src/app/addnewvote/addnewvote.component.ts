7beeb99dd17cbe569a500b61f3f5b60aa523ee46
Rana <rana.ahmed4047@gmail.com>
2025-05-06 16:54:08 +0300

    removeenv

--- src/app/addnewvote/addnewvote.component.ts
+++ src/app/addnewvote/addnewvote.component.ts
@@ -1,21 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Location } from '@angular/common';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-addnewvote',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './addnewvote.component.html',
   styleUrl: './addnewvote.component.css'
 })
 export class AddnewvoteComponent {
-  submitForm(ev : any){
-    ev.preventDefault()
-  }
+
+  options: string[] = ['']; // Start with one input
 
   constructor(private location: Location) {}
 
   goBack() {
     this.location.back(); // Goes one step back in history
   }
+
+
+  addOption() {
+    this.options.push(''); // Add another empty option
+  }
+
+  
+  submitForm(ev : any){
+    ev.preventDefault()
+  }
+
+  updateOption(index: number, value: string) {
+    this.options[index] = value;
+  }
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
-export const environment = {
-    production: false,
-    firebaseConfig: {
-        apiKey: "AIzaSyDZsJJYeBeuGzM_bG_pQcVJ45BxMm3Wuvk",
-        authDomain: "my--angular-auth.firebaseapp.com",
-        projectId: "my--angular-auth",
-        storageBucket: "my--angular-auth.firebasestorage.app",
-        messagingSenderId: "51671505157",
-        appId: "1:51671505157:web:613ff3ebef1555f705bf6c"
-    }
+// export const environment = {
+//     production: false,
+//     firebaseConfig: {
+//         apiKey: "AIzaSyDZsJJYeBeuGzM_bG_pQcVJ45BxMm3Wuvk",
+//         authDomain: "my--angular-auth.firebaseapp.com",
+//         projectId: "my--angular-auth",
+//         storageBucket: "my--angular-auth.firebasestorage.app",
+//         messagingSenderId: "51671505157",
+//         appId: "1:51671505157:web:613ff3ebef1555f705bf6c"
+//     }
     
-};
+// };
